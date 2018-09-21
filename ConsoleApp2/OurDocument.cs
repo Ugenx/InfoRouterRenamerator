30bb7bf5192465762dddbee8ca4b2066a7a28022
Jeremy Stafford <dasjestyr@gmail.com>
2018-09-20 20:49:48 -0700

    initial commit

---
+++ ConsoleApp2/OurDocument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace InfoRouterRenamerator
+{
+    public class OurDocument
+    {
+        private const string FilenameFormat = "{0}_{1}_{2}";
+        private const string OriginalFolderPath = "\\NRG_DYNAMICS";
+
+        public string LoadNumber { get; set; }
+
+        public string DriverName { get; set; }
+
+        public string OriginalFileName { get; set; }
+
+        public string RemoteDocumentPath => $"{OriginalFolderPath}\\{OriginalFileName}";
+
+        public string LocalFileName => GetDownloadFileName();
+
+        public string GetDownloadFileName()
+        {
+            // use "noload" if na else use whatever is in that field
+            var prefix = LoadNumber.Equals("NA", StringComparison.OrdinalIgnoreCase)
+                ? "NOLOAD"
+                : LoadNumber;
+
+            return string.Format(FilenameFormat,
+                prefix,
+                DriverName,
+                OriginalFileName);
+        }
+
+        public static OurDocument LoadFromElement(XElement element)
+        {
+            var doc = new OurDocument();
+            var ovs = element.Descendants("propertyset").SingleOrDefault(set => set.Attribute("Name")?.Value == "OVS");
+            if (ovs == null) return null;
+            var targetRow = ovs.Elements("propertyrow").FirstOrDefault(row => row.Attribute("LOADNUMBER") != null);
+            if (targetRow == null) return null;
+
+            doc.DriverName = targetRow.Attribute("DRIVERNAME")?.Value;
+            doc.LoadNumber = targetRow.Attribute("LOADNUMBER")?.Value;
+            doc.OriginalFileName = element.Attribute("Name")?.Value;
+            return doc;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using TestServiceThing;
+
+namespace InfoRouterRenamerator
+{
+    class Program
+    {
+        const string sourceFolder = "\\NRG_DYNAMICS";
+
+        private string _ticket;
+        private srvSoapClient _client;
+
+        static int Main(string[] args)
+        {
+            var p = new Program();
+
+            var user = args[0];
+            var pass = args[1];
+            var localDestination = args[2];
+
+            if (args.Length < 3 || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass) ||
+                string.IsNullOrEmpty(localDestination))
+                return 1;
+
+            p.RunThing(user, pass, localDestination).GetAwaiter().GetResult();
+            return 0;
+        }
+
+        public async Task RunThing(string user, string pass, string localDestinationFolder)
+        {
+            ConfigureClient(user, pass);
+            
+            var docs = await _client.GetDocumentsAsync(_ticket, sourceFolder, true, false, false, false).ConfigureAwait(false);
+
+            var documents = docs.Elements("document");
+
+            var targetDocs = documents.Select(OurDocument.LoadFromElement).ToList();
+            await DownloadFiles(targetDocs, localDestinationFolder).ConfigureAwait(false);
+            await MoveRemoteFiles(targetDocs, $"{sourceFolder}\\Processed").ConfigureAwait(false);
+
+        }
+
+        private void ConfigureClient(string user, string pass)
+        {
+            var binding = new BasicHttpsBinding {MaxReceivedMessageSize = 0x3B9ACA00};
+            var endpoint = new EndpointAddress("https://scans.omnitracs.com/InfoRouter/srv.asmx");
+            _client = new srvSoapClient(binding, endpoint);
+
+            var authResp = _client.AuthenticateUserAsync(user, pass).Result;
+            _ticket = authResp.Attribute("ticket").Value;
+        }
+
+        private async Task MoveRemoteFiles(IEnumerable<OurDocument> sourceDocs, string destination)
+        {
+            foreach (var doc in sourceDocs)
+            {
+                var result = await _client
+                    .CopyAsync(_ticket, doc.RemoteDocumentPath, $"{destination}\\{doc.LocalFileName}")
+                    .ConfigureAwait(false);
+                Debug.WriteLine(result);
+            }
+        }
+
+        private async Task DownloadFiles(IEnumerable<OurDocument> docs, string localDestinationFolder)
+        { 
+            // test first
+            foreach (var doc in docs)
+            {
+                var file = await _client.DownloadDocumentAsync(_ticket, doc.RemoteDocumentPath);
+                Debug.WriteLine($"Downloaded {doc.OriginalFileName}");
+                await File
+                    .WriteAllBytesAsync(Path.Combine(localDestinationFolder, doc.LocalFileName), file.DownloadDocumentResult)
+                    .ConfigureAwait(false);
+                Debug.WriteLine($"Saved {doc.LocalFileName}");
+            }
+        }
+    }
+}
